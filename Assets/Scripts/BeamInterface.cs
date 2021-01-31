a7ed60fe9d045b17a7bbe8add14e7d7118d082e6
MuhenderRaj <rkmr02@gmail.com>
2021-02-01 04:43:02 +0530

    Added script to control beam properties
    Also removed "NewBehaviorScript" that caused errors and didn't allow me to attach my script to the beam

---
+++ Assets/Scripts/BeamInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this to set properties of the GravBeam like length (under development)
+/// and whether it is free or hooked (under development).
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class BeamInterface : MonoBehaviour {
+    public float length;
+    public bool isHooked;
+
+    ParticleSystem beam;
+
+    // Start is called before the first frame update
+    void Start() {
+        beam = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
