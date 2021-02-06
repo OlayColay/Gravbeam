e5084966cde4aff47abf501048ddd558083d0804
chris-lok <chrislok2018@gmail.com>
2021-02-05 17:44:44 -0800

    ??

---
+++ Assets/Scripts/BeamInterface.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this to set properties of the GravBeam like length (under development)
+/// and whether it is free or hooked (under development).
+/// </summary>
+[ExecuteInEditMode]
+[RequireComponent(typeof(ParticleSystem))]
+public class BeamInterface : MonoBehaviour {
+    [Tooltip("The length of the beam, as shown by the gizmo")]
+    public float length = 5f;
+    [Tooltip("Whether the beam is hooked onto a surface")]
+    public bool isHooked = false;
+    [Tooltip("Speed of the beam particles when hooked")]
+    public float hookedSpeed;
+    [Tooltip("Speed of the beam particles when free")]
+    public float freeSpeed;
+
+    public Gradient freeColor;
+
+    public Gradient hookedColor;
+
+    ParticleSystem beam;
+
+    // Start is called before the first frame update
+    void Start() {
+        beam = GetComponent<ParticleSystem>();
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.DrawLine(transform.position, transform.position + length * transform.forward);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ParticleSystem.MainModule mainParams = beam.main;
+        ParticleSystem.NoiseModule noise = beam.noise;
+        ParticleSystem.TrailModule trail = beam.trails;
+
+        ParticleSystem.MinMaxCurve lifetime = mainParams.startLifetime;
+        ParticleSystem.MinMaxCurve speed = mainParams.startSpeed;
+
+        if (isHooked) {
+            speed = hookedSpeed;
+            noise.enabled = false;
+            trail.dieWithParticles = false;
+
+            ParticleSystem.MinMaxGradient color = trail.colorOverLifetime;
+            color.gradient = hookedColor;
+            trail.colorOverLifetime = color;
+        }
+        else {
+            speed = freeSpeed;
+            noise.enabled = true;
+            trail.dieWithParticles = true;
+
+            ParticleSystem.MinMaxGradient color = trail.colorOverLifetime;
+            color.gradient = freeColor;
+            trail.colorOverLifetime = color;
+        }
+
+        lifetime.constant = length / speed.constant;
+
+        mainParams.startLifetime = lifetime;
+        mainParams.startSpeed = speed;
+
+
+
+        mainParams.startLifetime = lifetime;
+        mainParams.startSpeed = speed;
+
+    }
+}
---
+++ Assets/Scripts/ValentinBaseScript.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValentinBaseScript : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform gunTip, camera, player;
+    private float maxDistance = 10f;
+    private SpringJoint joint;
+    private bool isGrappling;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        isGrappling = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            StartGrapple();
+        }
+        else if (Input.GetAxis("Horizontal") <= 0)
+        {
+            StopGrapple();
+        }
+
+    }
+
+    //Called after Update
+    void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    /// <summary>
+    /// Call whenever we want to start a grapple
+    /// </summary>
+    void StartGrapple()
+    {
+        
+        if (isGrappling)
+            return;
+        RaycastHit hit;
+        if (Physics.Raycast(camera.position, camera.right, out hit, maxDistance, whatIsGrappleable))
+        {
+            Debug.Log("here");
+            isGrappling = true;
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+            //The distance grapple will try to keep from grapple point. 
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+
+            //Adjust these values to fit your game.
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+
+            lr.positionCount = 2;
+            currentGrapplePosition = gunTip.position;
+        }
+    }
+
+
+    /// <summary>
+    /// Call whenever we want to stop a grapple
+    /// </summary>
+    void StopGrapple()
+    {
+        isGrappling = false;
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    private Vector3 currentGrapplePosition;
+
+    void DrawRope()
+    {
+        //If not grappling, don't draw rope
+        if (!joint) return;
+
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
+
+        lr.SetPosition(0, gunTip.position);
+        lr.SetPosition(1, currentGrapplePosition);
+    }
+
+    public bool IsGrappling()
+    {
+        return joint != null;
+    }
+
+    public Vector3 GetGrapplePoint()
+    {
+        return grapplePoint;
+    }
+
+}
