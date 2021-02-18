68986bf812bd3d04565d65fcb550f349ce3762ed
Cole Strain <colestrain2000@gmail.com>
2021-02-17 19:12:00 -0800

    Start work on CameraTriggers

---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    [SerializeField] private Vector2 startingPosition;
+    [SerializeField] private float ySize = 10f;
+    [SerializeField] private bool scrollX = false;
+    [SerializeField] private bool scrollY = false;
+
+    private CameraScript cameraScript;
+
+    void Awake()
+    {
+        cameraScript = GameObject.FindGameObjectWithTag("Camera").GetComponent<CameraScript>();
+    }
+
+    void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            cameraScript.NewAngle(startingPosition, ySize, scrollX, scrollY);
+        }
+    }
+}
