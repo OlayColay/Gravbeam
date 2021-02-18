e916c964edd86e953b0f425b0b8ca0b9ad19a6f3
MuhenderRaj <rkmr02@gmail.com>
2021-02-18 09:24:33 +0530

    Working on camera window script

--- Assets/Scripts/MuheScripts/ChangeCamWindow.cs
+++ Assets/Scripts/MuheScripts/ChangeCamWindow.cs
@@ -7,22 +7,35 @@ using UnityEngine;
 /// </summary>
 public class ChangeCamWindow : MonoBehaviour {
 
+    public float speedOfChange = 5f;
+    public AnimationCurve speedVariation;
+
     bool followAlongX;
     bool followAlongY;
+    
     Vector2 windowCenterPos;
-    float windowSizeX;
-    float windowSizeY;
 
-    Transform cameraPos;
+    Transform cameraTransform;
+    Transform playerTransform;
+
+    Camera cam;
+
+    bool moveDone = true;
+
+    IEnumerator currentCoroutine;
 
     // Start is called before the first frame update
     void Start() {
-            
+        cameraTransform = transform;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (moveDone) {
+
+        }
     }
 
     /// <summary>
@@ -30,10 +43,37 @@ public class ChangeCamWindow : MonoBehaviour {
     /// </summary>
     /// <param name="followAlongX">Whether the camera follows the player's x position</param>
     /// <param name="followAlongY">Whether the camera follows the player's y position</param>
-    /// <param name="windowCenterPos">the position (Vector2) of the center of the window</param>
+    /// <param name="windowCenterPos">The position (Vector2) of the center of the window</param>
     /// <param name="windowSizeY">The height of the window (aspect ratio 16:9)</param>
-    public void changeWindow(bool followAlongX, bool followAlongY, Vector2 windowCenterPos, float windowSizeY) {
+    public void changeWindow(bool followAlongX, bool followAlongY, Vector2 windowCenterPos, float windowSizeY, Vector2 camBounds) {
         this.followAlongX = followAlongX;
+        this.followAlongY = followAlongY;
+        this.windowCenterPos = windowCenterPos;
+
+        cam.orthographicSize = windowSizeY / 2;
+
+        StartCoroutine(moveToNewWindow());
     }
 
+    IEnumerator moveToNewWindow() {
+        Vector2 camPos = cameraTransform.position;
+        while ((camPos - windowCenterPos).magnitude > 0.01) {
+            cameraTransform.position = Vector2.MoveTowards(cameraTransform.position, windowCenterPos, speedVariation.Evaluate(speedOfChange * Time.deltaTime));
+            yield return null;
+        }
+    }
+
+    IEnumerator followPlayer() {
+        while (true) {
+            // TODO implement camera bounds
+            if (followAlongX) {
+                cameraTransform.position.Set(playerTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
+            }
+
+            if (followAlongY) {
+                cameraTransform.position.Set(cameraTransform.position.x, playerTransform.position.y, cameraTransform.position.z);
+            }
+            yield return null;
+        }
+    }
 }
