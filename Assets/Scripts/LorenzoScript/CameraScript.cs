724e4beeece0fb4b8dd8e81a1c4067a07b472e53
lorenzoc25 <lorenzocao25@gmail.com>
2021-02-17 18:43:09 -0800

    chsnged my csm script to follow x only

--- Assets/Scripts/LorenzoScript/CameraScript.cs
+++ Assets/Scripts/LorenzoScript/CameraScript.cs
@@ -19,7 +19,7 @@ public class CameraScript : MonoBehaviour
         Vector3 temp = new Vector3(0,0,-1);
         temp = playerTransform.position;
         temp.x = playerTransform.position.x;
-        temp.y = playerTransform.position.y;
+        temp.y = 0;
         temp.z = -1; // to show the scene
 
 
