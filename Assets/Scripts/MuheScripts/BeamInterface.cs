bb4f6ab058cebfc586492bb5a68709984590074b
MuhenderRaj <rkmr02@gmail.com>
2021-03-04 21:05:24 +0530

    Commenting in BeamInterface
    Now it's less of a mess

--- Assets/Scripts/MuheScripts/BeamInterface.cs
+++ Assets/Scripts/MuheScripts/BeamInterface.cs
@@ -18,31 +18,25 @@ public class BeamInterface : MonoBehaviour {
     public float hookedSpeed;
     [Tooltip("Speed of the beam particles when free")]
     public float freeSpeed;
-
+    [Tooltip("The color of the beam when not pulling on a surface")]
     public Gradient freeColor;
-
+    [Tooltip("The color of the beam when pulling on a surface")]
     public Gradient hookedColor;
 
-    //public AnimationCurve sizeOverTime;
-
+    // The particle system interface
     ParticleSystem beam;
-    bool wasHooked;
-    float lengthBuf;
 
-    float transverseScale;
-    float longScale;
-    float scaleFactor;
+    // Whether beam was hooked before this update cycle
+    bool wasHooked;
+
+    // The length reference for unit z-scale of the beam
+    float lengthUnit;
 
     // Start is called before the first frame update
     void Awake() {
         beam = GetComponent<ParticleSystem>();
         wasHooked = isHooked;
-        lengthBuf = length;
-
-        transverseScale = transform.localScale.y;
-        longScale = transform.localScale.z;
-
-        scaleFactor = longScale / transverseScale;
+        lengthUnit = length;
     }
 
     void OnDrawGizmosSelected() {
@@ -51,70 +45,61 @@ public class BeamInterface : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
-        transverseScale = transform.localScale.y;
-        
-
+        // Various tabs of the particle system module
         ParticleSystem.MainModule mainParams = beam.main;
         ParticleSystem.NoiseModule noise = beam.noise;
         ParticleSystem.TrailModule trail = beam.trails;
         ParticleSystem.VelocityOverLifetimeModule velocity = beam.velocityOverLifetime;
 
+        // Various parts of the main module
         ParticleSystem.MinMaxCurve lifetime = mainParams.startLifetime;
         ParticleSystem.MinMaxCurve speed = mainParams.startSpeed;
+
+        // Various parts of the trail module
         ParticleSystem.MinMaxGradient color = trail.colorOverLifetime;
 
+        // Switch beams
         if (isHooked && !wasHooked) {
             beam.Stop();
             beam.Clear();
 
-            speed.constant = hookedSpeed;
-            noise.enabled = false;
-            trail.dieWithParticles = false;
-            velocity.enabled = true;
-            color.gradient = hookedColor;
-            lifetime.constant = lengthBuf / hookedSpeed;
-            mainParams.startSpeed = speed;
-            wasHooked = true;
-            mainParams.startLifetime = lifetime;
-            beam.Play();
+            speed.constant = hookedSpeed;                   // Set speed of beam particles
+            noise.enabled = false;                          // Disable noise
+            trail.dieWithParticles = false;                 // Give beam a thick look
+            velocity.enabled = true;                        // Do things with the velocity, like spiraling
+            color.gradient = hookedColor;                   // Set color of beam
+            lifetime.constant = lengthUnit / hookedSpeed;   // Calculate lifetime to give the beam its unit z-scale length
+            mainParams.startSpeed = speed;                  // Put the speed back into the module
+            wasHooked = true;                               // Beam was hooked in this update
+            mainParams.startLifetime = lifetime;            // Put the lifetime back into the module
+            beam.Play();                                    // Play the beam particle simulation
         }
         else if (!isHooked && wasHooked) {
             beam.Stop();
             beam.Clear();
 
-            speed.constant = freeSpeed;
-            noise.enabled = true;
-            trail.dieWithParticles = true;
-            velocity.enabled = false;
-            color.gradient = freeColor;
-            lifetime.constant = (lengthBuf + 1.5f) / freeSpeed;
-            mainParams.startSpeed = speed;
-            wasHooked = false;
-            mainParams.startLifetime = lifetime;
-            beam.Play();
+            speed.constant = freeSpeed;                             // Set speed of beam particles
+            noise.enabled = true;                                   // Enable noise
+            trail.dieWithParticles = true;                          // Give beam a thin look
+            velocity.enabled = false;                               // Deactivate velocity stuff
+            color.gradient = freeColor;                             // Set color of beam
+            lifetime.constant = (lengthUnit + 1.5f) / freeSpeed;    // Calculate lifetime to give the beam its unit z-scale length
+            mainParams.startSpeed = speed;                          // Put the speed back into the module
+            wasHooked = false;                                      // Beam was unhooked in this update
+            mainParams.startLifetime = lifetime;                    // Put the lifetime back into the module
+            beam.Play();                                            // Play the beam particle simulation
         }
-
-        //if(lengthBuf != length) {
-        //    lifetime.constant = (11.5f) / speed.constant;
-        //    mainParams.startLifetime = lifetime;
-        //}
-
-        //Debug.Log(transform.localScale.z * length);
-
-        //transform.localScale.Set(transform.localScale.x, transform.localScale.y, transform.localScale.z * length / lengthBuf);
         
-        trail.colorOverLifetime = color;
-
-        
+        trail.colorOverLifetime = color;    // Put the color back into the module
     }
 
     public void SetLength(float length) {
 
         float xScale = 1;
         float yScale = 1;
-        if (length < 0.1f)
-        {
+
+        // If length gets too close to zero, set length so that scale doesn't become zero
+        if (length < 0.1f) {
             length = 0.1f;
             beam.Stop();
 
@@ -123,7 +108,6 @@ public class BeamInterface : MonoBehaviour {
         }
 
         this.length = length;
-        Debug.Log(length / lengthBuf);
-        transform.localScale = new Vector3(xScale, yScale, length / lengthBuf);
+        transform.localScale = new Vector3(xScale, yScale, length / lengthUnit);
     }
 }
