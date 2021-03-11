9b0b11285537c4f0ee8fc6a73fdd034ed164cf3f
MuhenderRaj <rkmr02@gmail.com>
2021-03-11 07:47:50 +0530

    Pause menu script
    I used default input actions for eventsystem

---
+++ Assets/Scripts/MuheScripts/ButtonListeners.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonListeners : MonoBehaviour {
+    [SerializeField] int n;
+    public void OnClickResume() {
+        n++;
+        transform.parent.gameObject.SetActive(false);
+        Debug.Log(n);
+    }
+
+    public void OnClickRestart() {
+        // Restart level
+    }
+
+    public void OnClickOptions() {
+        // Bring up options menu
+    }
+
+    public void OnClickQuit() {
+        // Quit to main menu
+    }
+}
