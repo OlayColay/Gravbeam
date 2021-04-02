7fb5432e2cec015fb42911c3b6c9ce269fef1a61
Cole Strain <colestrain2000@gmail.com>
2021-04-01 19:13:47 -0700

    Added Goal as a trigger zone that loads a specified level index

---
+++ Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGoal : MonoBehaviour
+{
+    [Tooltip("The build index of the next level's scene. These can be found/edited in build settings")]
+    [SerializeField] private int nextLevelNumber;
+
+    void OnDrawGizmos()
+    {
+        GetComponent<BoxCollider2D>().offset = Vector2.zero;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider2D>().size);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerPrefs.SetInt("currentLevel", nextLevelNumber);
+            
+            // Debug.Log("Latest Level was " + PlayerPrefs.GetInt("latestLevel", 0));
+            if (nextLevelNumber > PlayerPrefs.GetInt("latestLevel", 0))
+            {
+                PlayerPrefs.SetInt("latestLevel", nextLevelNumber);
+            }
+            // Debug.Log("Latest Level is now " + PlayerPrefs.GetInt("latestLevel", 0));
+
+            SceneManager.LoadScene(nextLevelNumber, LoadSceneMode.Single);
+        }
+    }
+}
