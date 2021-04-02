7761fd8e88e867bfb3b1fcebbd66a8be4de76dfb
AndrewYiBC <bladecrazydf@gmail.com>
2021-04-02 10:10:03 +0800

    Respawn

---
+++ Assets/Scripts/AndrewScripts/Respawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform spawnPoint;
+    Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = spawnPoint.transform.position;
+        rb.velocity = Vector2.zero;
+    }
+}
