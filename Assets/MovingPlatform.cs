439dd7724ba1f5b31dbaaf00e8beb28124db0757
OlayColay <colestrain2000@gmail.com>
2021-04-17 19:27:34 -0700

    DOTween and started MovingPlatform

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Vector3[] positions;
+    [SerializeField] private float timeBetweenEachPosition = 1f;
+    private int curPosIndex = 1;
+    private float distance = 0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, positions[curPosIndex], distance / Time.fixedDeltaTime / timeBetweenEachPosition);
+
+        if (transform.position == positions[curPosIndex])
+        {
+            curPosIndex = (curPosIndex + 1 < positions.Length) ? curPosIndex + 1 : 0;
+            distance = Vector3.Distance(transform.position, positions[curPosIndex]);
+        }
+    }
+}
