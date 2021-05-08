0e76a51699688051310ed18077c1cc2bc6b3c89c
Cole Strain <colestrain2000@gmail.com>
2021-05-07 18:51:41 -0700

    Gamepad menu compatibility

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject firstSelectedButton;
+
+    private GameObject previousSelectedButton;
+
+    private void OnEnable() 
+    {
+        if (EventSystem.current != null && Gamepad.current != null)
+        {
+            previousSelectedButton = EventSystem.current.currentSelectedGameObject;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+        }
+    }
+    
+    private void OnDisable() 
+    {
+        if (EventSystem.current != null && Gamepad.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(previousSelectedButton);
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject firstSelectedButton;
+
+    private void OnEnable() 
+    {
+        if (EventSystem.current != null && Gamepad.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+        }
+    }
+}
