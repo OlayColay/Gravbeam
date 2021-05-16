ff1a79c85175d8c69c0f9756e23d063eef564a3d
Cole Strain <colestrain2000@gmail.com>
2021-05-15 19:33:52 -0700

    Door almost not stuck

---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private float animationTime = 1f;
+    [SerializeField] private float doorLength = 1f;
+
+    void OnDrawGizmos()
+    {
+        GetComponent<BoxCollider2D>().offset = Vector2.zero;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider2D>().size);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" || other.tag == "Rock")
+        {
+            Transform door = transform.GetChild(0);
+            door.DOLocalMoveY(doorLength, animationTime);
+        }
+    }
+}
