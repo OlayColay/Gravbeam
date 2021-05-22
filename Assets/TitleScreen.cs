fb9ca9de0b6a553d1ed0de5ea8662b73deb55947
Cole Strain <colestrain2000@gmail.com>
2021-05-22 00:03:55 -0700

    Title Screen!

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] Transform skreech;
+    [SerializeField] private GameObject firstSelectedButton;
+
+    private void OnEnable() 
+    {
+        if (EventSystem.current != null && Gamepad.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            StartCoroutine(SelectFirstButton());
+        }
+    }
+
+    private IEnumerator SelectFirstButton()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
