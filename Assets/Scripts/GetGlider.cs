330bb5838499ef0b0d708c59148ff6748c00e1fd
Cole Strain <colestrain2000@gmail.com>
2021-05-27 00:06:53 -0700

    Level 1 Work

--- Assets/Scripts/GetGlider.cs
+++ Assets/Scripts/GetGlider.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class GetGlider : MonoBehaviour
 {
+    private void Start() 
+    {
+        if (PlayerPrefs.GetInt("gotGlider", 0) == 1)
+        {
+            FindObjectOfType<PlatformerCharacter2D>().hasGlider = true;
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Player")
         {
             other.GetComponent<PlatformerCharacter2D>().hasGlider = true;
+            PlayerPrefs.SetInt("gotGlider", 1);
             Destroy(gameObject);
         }
     }
