39c12912225d6259a715f677ff3c2f4e6ea51ea5
Cole Strain <colestrain2000@gmail.com>
2021-05-26 22:34:54 -0700

    Fixes for Options menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -16,8 +16,14 @@ public class OptionsMenu : MonoBehaviour
         {
             previousSelectedButton = EventSystem.current.currentSelectedGameObject;
             EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+            StartCoroutine(SelectFirstButton());
         }
+    }
+
+    private IEnumerator SelectFirstButton()
+    {
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
     }
     
     private void OnDisable() 
